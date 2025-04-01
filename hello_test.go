f08e8951b7f9073e108767b7a30e023a127ad04a
Carlos Eugenio Torres Mota <teugenio@tresguerras.com.mx>
2025-04-01 17:13:34 -0600

    test refactor into subtesting and file fixes

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Carlos")
-	want := "Hello, Carlos"
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Carlos")
+		want := "Hello, Carlos"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
 
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+
 }
