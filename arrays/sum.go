027d3bf1edbe234d53e8b129cf6241cb81979b24
Carlos Eugenio Torres Mota <teugenio@tresguerras.com.mx>
2025-04-03 18:26:34 -0600

    created a helper funciton to validate if the test was correct and making a slice of a slice

--- arrays/sum.go
+++ arrays/sum.go
@@ -9,11 +9,25 @@ func Sum(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
+	//Could be done using  append on an empty int slice
 	lengthOfNumbers := len(numbersToSum)
 	sums := make([]int, lengthOfNumbers)
 
 	for i, numbers := range numbersToSum {
 		sums[i] = Sum(numbers)
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 	return sums
 }
