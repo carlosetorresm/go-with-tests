827c559b13ae848210449462c231738dca54429a
Carlos Eugenio Torres Mota <teugenio@tresguerras.com.mx>
2025-04-08 14:15:32 -0600

    mocking our sleeper struct and implementing it as di

--- mocking/main.go
+++ mocking/main.go
@@ -12,14 +12,25 @@ const (
 	coundownStart = 3
 )
 
-func Countdown(w io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(w io.Writer, sleeper Sleeper) {
 	for i := coundownStart; i > 0; i-- {
 		fmt.Fprintln(w, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(w, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
