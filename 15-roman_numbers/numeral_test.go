1786e59ed350009a253cbc933dc41b3e32b70966
Carlos Eugenio Torres Mota <teugenio@tresguerras.com.mx>
2025-04-09 21:34:49 -0600

    making enought code to satisfy simple tests, making assumptions on the next step by substracting

---
+++ 15-roman_numbers/numeral_test.go
@@ -0,0 +1,30 @@
+package romannumbers
+
+import "testing"
+
+// Roman Numeral from Arabic Numbers
+
+func TestRomanNumerals(t *testing.T) {
+	cases := []struct {
+		Descripcion string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
+		{"9 gets converted to IX", 9, "IX"},
+	}
+	for _, test := range cases {
+		t.Run(test.Descripcion, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+
+			if got != test.Want {
+				t.Errorf("got %q, want %q", got, test.Want)
+			}
+		})
+	}
+
+}
